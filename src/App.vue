9e840e0b7b2fe7607d1cef2403a39357b66b4b50
Josep Ballester <josep.ballester@ext.mercedes-benz.io>
2021-01-19 11:47:45 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -6,23 +6,16 @@
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
-    HelloWorld
-  }
-};
+    HelloWorld,
+  },
+}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './index.css';
 </style>
